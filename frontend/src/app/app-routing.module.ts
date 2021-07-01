aa7269f4dbc42d2f1825e96d83dc209d0ac0adc6
beginerbeginer <miyabi6125@gmail.com>
2021-07-02 02:47:03 +0900

    編集：ルーティング

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+/**
+ *​「pathMatch: 'full'」で空のパスと完全に一致するURLを、
+ * パスが'/employees'であるルートにリダイレクト
+ *
+ * '/employees'にリクエスト送った場合のみemployeesが表示される
+*/
+const routes: Routes = [
+  { path: 'employees', component: EmployeeListComponent },
+  { path: '', redirectTo: 'employees', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
