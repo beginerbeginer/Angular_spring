7792ebb9d5f82cd69115ab40b536cd046f1e40c7
beginerbeginer <miyabi6125@gmail.com>
2021-07-06 23:49:13 +0900

    編集：emploee validation

--- backend/src/main/java/com/example/demo/model/Employee.java
+++ backend/src/main/java/com/example/demo/model/Employee.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,13 +25,14 @@ public class Employee implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long Id;
 
-	@Column(name = "first_name")
+	@Column(name = "first_name", nullable = false)
 	private String firstName;
 
-	@Column(name = "last_name")
+	@Column(name = "last_name", nullable = false)
 	private String lastName;
 
-	@Column(name = "email_id")
+	@Column(name = "email_id", nullable = false)
+	@Size(min = 4, max = 20)
 	private String emailId;
 
 }
